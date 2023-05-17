f65f8c185686cdca0da82fa9cf12f9d7ef471e07
Handsome <pratikbanger05@gmail.com>
2023-05-17 14:27:58 +0530

    minor modal updates

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -89,7 +89,7 @@ const Blogs = () => {
 
             {/* Confirm modal */}
             <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                <div className="modal-dialog">
+                <div className="modal-dialog modal-dialog-centered">
                     <div className="modal-content">
                         <div className="modal-header">
                             <h1 className="modal-title fs-5" id="staticBackdropLabel">Delete Blog</h1>
--- src/components/MyBlogs.jsx
+++ src/components/MyBlogs.jsx
@@ -76,7 +76,7 @@ const MyBlogs = () => {
 
                     {/* Confirm modal */}
                     <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                        <div className="modal-dialog">
+                        <div className="modal-dialog modal-dialog-centered">
                             <div className="modal-content">
                                 <div className="modal-header">
                                     <h1 className="modal-title fs-5" id="staticBackdropLabel">Delete Blog</h1>
